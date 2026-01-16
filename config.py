fa0c64017d0aa9f0ee737638cdd937b2174cf982
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-16 11:32:05 +0530

    Add configuration settings for the application

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "mysecretkey"
+
+    # SQLite database (local + Render compatible)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "sqlite:///" + os.path.join(basedir, "ecommerce.db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
