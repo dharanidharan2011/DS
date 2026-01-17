e943d351249b5a3aa3a03e2ee030cea383aa2bdb
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-17 15:08:07 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder="templates", static_folder="static")  # <-- here
+    app = Flask(__name__, template_folder="template", static_folder="static")  # <-- here
     app.config.from_object("config.Config")
 
     db.init_app(app)
