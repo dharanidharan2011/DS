1f8d59728df2c7034eb110f32f64dd2c41992625
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-17 15:14:02 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder="template", static_folder="static")  # <-- here
+    template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../template")
+    static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
+    
+    app = Flask(__name__, template_folder=template_path, static_folder=static_path)
     app.config.from_object("config.Config")
 
     db.init_app(app)
 
-    # Import and register your routes
     from .routes import main
     app.register_blueprint(main)
 
