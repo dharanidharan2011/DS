a166f1d81a3f464bb6180cf4228680d8f5610970
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-19 19:46:46 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-    template_path = os.path.join(base_dir, "templates")
+    template_path = os.path.join(base_dir, "template")
     static_path = os.path.join(base_dir, "static")
 
     app = Flask(__name__, template_folder=template_path, static_folder=static_path)
