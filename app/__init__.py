5accb4d9b8cdaf955bbb0488b9314112b342431f
dharanidharan2011 <seodm.archon@gmail.com>
2026-01-19 19:43:08 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../template")
-    static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
-    
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    template_path = os.path.join(base_dir, "templates")
+    static_path = os.path.join(base_dir, "static")
+
     app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+
     app.config.from_object("config.Config")
 
     db.init_app(app)
